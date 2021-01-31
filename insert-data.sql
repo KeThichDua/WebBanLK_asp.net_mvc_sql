059d40ac5d0bc20dc49b8335f3e0c0b7a8606ca6
KeThichDua <buihoanglam99@gmail.com>
2021-02-01 00:31:02 +0700

    commit dau tien

---
+++ insert-data.sql
@@ -0,0 +1,264 @@
+-- san pham: MaSP, TenSP, SoLuong, DonGia, MoTa, GiaKm, URLAnh, MaLoaiSP
+insert into sanpham values 
+('sp1' , N'CPU Intel Xeon Silver 4110' , '12', '12999000' ,
+ N'Dòng sản phẩm chuyên biệt dành cho server/máy trạm
+ 8 nhân & 16 luồng
+ Xung nhịp: 2.1GHz (Cơ bản) / 3.0GHz (Boost)
+ Socket: LGA 3647
+ Hỗ trợ RAM ECC
+ Không kèm quạt tản nhiệt từ hãng
+ Không tích hợp sẵn iGPU' , '11999000' , N'~/images/sp1.jpg', 'lsp001');
+ insert into sanpham values 
+('sp2 ',N'CPU Intel Core i9-10940X', '14','23999000' ,
+N'CPU Core i thế hệ thứ 10 của Intel
+ 14 nhân & 28 luồng
+ Xung cơ bản: 3.3 GHz
+ Xung tối đa (boost): 4.6 GHz
+ Chạy tốt trên các mainboard socket 2066
+ Phù hợp cho những nhà sáng tạo nội dung ', '23199000',  N'~/images/sp2.jpg','lsp001');
+ insert into sanpham values 
+( 'sp3',N'CPU Intel Core i9-9900KF ', '15','11999000 ',
+ N'Phiên bản cắt giảm đi nhân đồ họa tích hợp của 9900K
+ Số nhân: 8
+ Số luồng: 16
+ Tốc độ cơ bản: 3.6 GHz
+ Tốc độ tối đa: 5.0 GHz
+ Cache: 16MB
+ Tiến trình sản xuất: 14nm ', '11199000',N'~/images/sp3.jpg' , 'lsp001');
+ insert into sanpham values 
+( 'sp4',N'CPU Intel Core i9-9900X' , '15', '14399000', 
+N'Socket: FCLGA2066
+Số lõi/luồng: 10/20-Tần số cơ bản/turbo: 3.5/4.4 GHz
+Bộ nhớ đệm: 19,5 MB
+Mức tiêu thụ điện: 165 W', '14199000',N'~/images/sp4.jpg' ,'lsp001');
+ insert into sanpham values 
+('sp5 ', N'CPU Intel Core i9-9900KF' , '23', '11999000', N'Phiên bản cắt giảm đi nhân đồ họa tích hợp của 9900K
+Số nhân: 8
+Số luồng: 16
+Tốc độ cơ bản: 3.6 GHz
+Tốc độ tối đa: 5.0 GHz
+Cache: 16MB
+Tiến trình sản xuất: 14nm', '11199000', N'~/images/sp5.jpg','lsp001');
+ insert into sanpham values 
+('sp6', N'Mainboard ASUS ProArt Z490-CREATOR 10G' , '12', '8899000', N'Chuẩn mainboard: ATX
+Socket: 1200, Chipset: Z490
+Hỗ trợ RAM: 4 khe DDR4, tối đa 128GB
+Lưu trữ: 3 x M.2 NVMe, 6 x SATA 3 6Gb/s
+Cổng xuất hình: Không', '8899000', N'~/images/sp6.jpg','lsp002');
+ insert into sanpham values 
+( 'sp7', N'Mainboard ASUS ProArt Z490-CREATOR 10G' , '12', '8899000', N'Chuẩn mainboard: ATX
+Socket: 1200, Chipset: Z490
+Hỗ trợ RAM: 4 khe DDR4, tối đa 128GB
+Lưu trữ: 2 x M.2 NVMe, 6 x SATA 3 6Gb/s
+Cổng xuất hình: 1 x HDMI, 1 x DP', '8199000', N'~/images/sp7.jpg','lsp002');
+ insert into sanpham values 
+( 'sp8', N'Mainboard ASUS ROG STRIX Z490-I GAMING' , '23', '7299000', N'Chuẩn mainboard: Mini ITX
+Socket: 1200, Chipset: Z490
+Hỗ trợ RAM: 2 khe DDR4, tối đa 64GB
+Lưu trữ: 2 x M.2 NVMe, 4 x SATA 3 6Gb/s
+Cổng xuất hình: 1 x HDMI, 1 x DisplayPort', '7099000', N'~/images/sp8.jpg','lsp002 ');
+ insert into sanpham values 
+('sp9 ', N'Mainboard ASUS ROG STRIX Z490-A GAMING' , '32', '6299000', N'Chuẩn mainboard: ATX
+Socket: 1200, Chipset: Z490
+Hỗ trợ RAM: 4 khe DDR4, tối đa 128GB
+Lưu trữ: 2 x M.2 NVMe, 6 x SATA 3 6Gb/s
+Cổng xuất hình: 1 x HDMI, 1 x DisplayPort', '6299000', N'~/images/sp9.jpg','lsp002');
+ insert into sanpham values 
+('sp10', N'Mainboard ASUS ROG STRIX Z490-I GAMING' , '12', '7299000', N'Chuẩn mainboard: Mini ITX
+Socket: 1200, Chipset: Z490
+Hỗ trợ RAM: 2 khe DDR4, tối đa 64GB
+Lưu trữ: 2 x M.2 NVMe, 4 x SATA 3 6Gb/s
+Cổng xuất hình: 1 x HDMI, 1 x DisplayPort', '7299000', N'~/images/sp10.jpg','lsp002');
+ insert into sanpham values 
+('sp11 ', N'DDRam 4 Kingston ECC 16GB 2400Mhz' , '15', '2999000', N' KSM24ED8/16ME', '2900000', N'~/images/sp11.jpg','lsp003');
+ insert into sanpham values 
+( 'sp12', N'Ram Desktop ANTECMEMORY 1S' , '33', '799000', N'Dòng RAM cơ bản nhất của Antecmemory
+Dung lượng: 8GB
+Kiểu: DDRam 4
+Bus: 2666 MHz
+Đóng gói: 1 x 8GB', '799000', N'~/images/sp12.jpg','lsp003' );
+ insert into sanpham values 
+( 'sp13', N'Ram Desktop ANTECMEMORY 1S ' , '32','799000', N'Dòng RAM cơ bản nhất của Antecmemory
+Dung lượng: 8GB
+Kiểu: DDRam 4
+Bus: 2666 MHz
+Đóng gói: 1 x 8GB', '799000', N'~/images/sp13.jpg','lsp003');
+ insert into sanpham values 
+('sp14', N'RAM Desktop ANTECMEMORY 1S 8GB' , '32', '779000', N'Kiểu Ram: DDR4
+Dung lượng: 8GB
+Bus: 2400MHz', '779000', N'~/images/sp14.jpg','lsp003');
+ insert into sanpham values 
+( 'sp15', N'RAM Desktop Gskill Trident Z Neo' , '12', '3000000', N'Dòng sản phẩm Gskill Trident Z Neo mới nhất của Gskill
+Phù hợp với nền tảng AMD
+Dung lượng: 2 x 8GB
+Thế hệ: DDR4
+Bus: 3600MHz', '3000000', N'~/images/sp15.jpg','lsp003');
+ insert into sanpham values 
+('sp16 ', N'Ổ cứng HDD Western Caviar Red 1TB 3.5 inch 5400RPM, SATA3 6Gb/s, 64MB Cache' , '31', '1500000', N'Được thiết kế đặc biệt để sử dụng trong các hệ thống NAS tối đa 8 khay
+Hỗ trợ tốc độ tải công việc lên tới 180 TB / năm.
+Hệ thống NAS phù hợp cho văn phòng nhỏ và sử dụng tại nhà liên tục 24/7', '1500000', N'~/images/sp16.jpg','lsp004');
+ insert into sanpham values 
+('sp17 ', N'HDD WD Gold' , '32', '5990000', N'Ổ cứng WD Gold 6TB chuyên dụng cho server -datacenter quy mô lớn.
+Dòng gắn trong 3.5 inch tốc độ quay 7200RPM,
+bộ đệm 128MB cache, tốc độ max 201 MB/s.', '5990000',  N'~/images/sp17.jpg','lsp004');
+ insert into sanpham values 
+('sp18', N'Ổ cứng HDD Western Purple 8TB 3.5 inch 7200RPM, Sata 3 ,256Mb Cache' , '43', '6000000', N'Ổ cứng gắn trong Western Purple 8TB - WD8 chuyên dụng cho thiết bị giám sát, đầu ghi camera, thích hợp sử dụng cho các cá nhân, văn phòng hoặc các doanh nghiệp với hệ thống lên đến 64 camera FullHD. Thông số kỹ thuật: dung lượng 8TB; giao tiếp SATA 3 (6Gb/s max); 256MB Cache, tốc độ quay 7200RPM. Công nghệ AllFrame tối ưu hóa, cải thiện hiệu suất, giảm tỉ lệ lỗi/khung.
+HDD WD PURPLE 8TB WD là dòng ổ cứng sản xuất ra để chuyên dùng cho thiết bị ghi hình Camera giám sát, chạy rất êm, giải nhiệt tốt hơn 70% so với ổ cứng PC thông thường, thích hợp trong hệ thống camera giám sát tại nhà và doanh nghiệp vừa và nhỏ.
+', '6000000', N'~/images/sp18.jpg','lsp004');
+ insert into sanpham values 
+( 'sp19', N'Ổ cứng HDD Nas Seagate Ironwolf Pro 12TB' , '32', '6000000', N'Seagate Ironwolf Pro hỗ trợ hệ thống NAS 1-16 bay
+Dung lượng 12TB
+Thiết kế đi kèm tính năng AgileArray tối ưu Tốc độ và độ bền cho ổ cứng.
+Cảm biến rung (RV) được tích hợp với mọi phiên bản giúp cho hệ thống NAS nhiều khay đĩa hoạt động êm ái nhất một cách có thể
+Cho phép khối lượng công việc người dùng sử dụng lên đến 300TB/ năm
+Quản lí điện năng tiên tiến giúp cho hệ thống của bạn hoạt động tốt mà vẫn tiết kiệm năng lượng.
+Hỗ trợ phần mềm Health Management IronWolf giúp quản lý ổ cứng trong hệ thống NAS để quản lý và phục hồi dữ liệu.', '5999000', N'~/images/sp19.jpg','lsp004');
+ insert into sanpham values 
+( 'sp20', N'Ổ cứng HDD Seagate SkyHawk 1TB 3.5 inch 5900RPM, SATA3 6GB/s, 64MB Cache' , '33', '1099000', N'Ổ cứng chuyên dụng cho các hệ thống Camera
+Hỗ trợ hệ thống giám sát tối ưu 24/7
+Trang bị bộ cảm biến duy trì hiệu năng hoạt động
+Hỗ trợ phát dẫn ATA
+Tiêu thụ điện năng rất thấp', '1099000', N'~/images/sp20.jpg','lsp004');
+ insert into sanpham values 
+('sp21', N'Ổ cứng SSD Samsung 860 EVO 250GB 2.5 inch SATA3' , '32', '1500000', N'Loại SSD: Giao tiếp Sata III
+Kích thước: 2.5 inch
+Dung lượng: 250GB
+Tốc độ đọc: 550MBps
+Tốc độ ghi: 520MBps
+Tổng dung lượng đã ghi (TBW): 150TB', '1499000', N'~/images/sp21.jpg','lsp005');
+ insert into sanpham values 
+('sp22 ', N'Ổ cứng SSD Plextor PX 256M8VC 256GB 2.5 inch SATA3' , '11', '999000', N'Tốc độ đọc: 560MB/s
+Tốc độ ghi: 520MB/s
+Chương trình cơ sở PlexNitro độc quyền tối ưu hóa hiệu suất trong chế độ SLC mà không chiếm dung lượng SSD
+Mã hóa toàn bộ ổ đĩa AES 256-bit dựa trên phần cứng đảm bảo bảo mật dữ liệu cao
+Công nghệ LDPC mới nhất đảm bảo độ chính xác của dữ liệu & độ tin cậy của SSD
+Tuổi thọ trung bình (MTBF) là 1,5 triệu giờ
+ECC 128 bit và Thuật toán giữ dữ liệu mạnh mẽ được sử dụng để đảm bảo dữ liệu được lưu trữ và truy xuất mà không gặp lỗi', '999000',  N'~/images/sp22.jpg','lsp005');
+ insert into sanpham values 
+('sp23', N'Ổ cứng SSD Samsung 860 EVO 500GB M.2 2280' , '11', '2000000', N'Loại SSD: Giao tiếp Sata III
+Kích thước: M.2 2280
+Dung lượng: 500GB
+Tốc độ đọc: 550MBps
+Tốc độ ghi: 520MBps
+Tổng dung lượng đã ghi (TBW): 150TB', '2000000', N'~/images/sp23.jpg','lsp005');
+ insert into sanpham values 
+( 'sp24', N'Ổ cứng SSD Adata SU650SN38 120G M.2 2280' , '11', '599000', N'Dung lượng: 120GB
+Tốc độ đọc: 550 MB/s
+Tốc độ ghi: 510 MB/s', '599000', N'~/images/sp24.jpg','lsp005');
+ insert into sanpham values 
+('sp25', N'Ổ cứng SSD Kingston UV500 240G M.2 2280' , '21', '400000', '', '400000', N'~/images/sp25.jpg','lsp005');
+ insert into sanpham values 
+('sp26', N'DVD Rewrite Asus SDRW-08D2S-U Ext USB' , '11', '779000', N'DVD Rewrite Asus SDRW-08D2S-U Ext USB, Disc Encryption tăng gấp đôi sự bảo mật bằng mật khẩu điều khiển và chức năng ẩn tập tin. Thiết kế thẩm mỹ, công nghệ vượt trội', '779000', N'~/images/sp26.jpg',' lsp006');
+ insert into sanpham values 
+( 'sp27', N'DVD Rewrite Asus SDRW-08D2S-U Ext USB 2.0' ,'11', '700000', '', '700000', N'~/images/sp27.jpg','lsp006');
+ insert into sanpham values 
+('sp28 ', N'DVDRw Asus 24D5MT P2G' , '11', '449000', '', '449000', N'~/images/sp28.jpg','lsp006');
+ insert into sanpham values 
+('sp29', N'DVDRw Asus 24D5MT P' , '12', '500000', '', '500000', N'~/images/sp29.jpg','lsp006 ');
+ insert into sanpham values 
+( 'sp30', N'DVD Asus 18X E818A9T Sata Đen Tray' , '21', '329000', '', '329000', N'~/images/sp30.jpg','lsp006');
+ insert into sanpham values 
+( 'sp31', N'Card màn hình Asus ROG STRIX RTX3090-O24G-GAMING' , '23', '55000000', N'Dung lượng bộ nhớ: 24Gb GDDR6X
+10496 CUDA Cores
+Core Clock: 1890 MHz (Boost Clock)
+Kết nối: DisplayPort 1.4a, HDMI 2.1
+Nguồn yêu cầu: 850W', '55000000', N'~/images/sp31.jpg','lsp007');
+ insert into sanpham values 
+('sp32', N'Vỏ Case Cooler Master MasterBox MB511 ARGB' , '32', '2000000', N'Hỗ trợ mainboard:ATX, Micro ATX, Mini ITX
+Radiator lắp đặt tối đa: 1x360/1x240/1x120mm
+Chiều cao tản nhiệt CPU tối đa: 165mm
+Chiều dài VGA: 410mm', '1900000', N'~/images/sp32.jpg','lsp008');
+ insert into sanpham values 
+( 'sp33', N'Nguồn FSP Power Supply HEXA 85+ Series Model HA450 Active PF' , '12', '1100000', N'Đạt tiêu chuẩn ATX12 v2.4 & EPS12 v2.92
+Hiệu suất cao ≧ 85%
+Active PFC ≧ 90%
+Tuân thủ chứng nhận 80 PLUS® Bronze
+Thiết kế đường 12V Single Rail
+Tụ điện hoàn toàn của Nhật Bản
+Quạt siêu êm 120mm
+Bảo vệ hoàn toàn: OCP, OVP, SCP, OPP, OTP
+An toàn toàn cầu được phê duyệt', '1100000', N'~/images/sp33.jpg','lsp009 ');
+
+-- loai sp: MaLoaiSP, TenLoai, MoTa, URLAnh
+insert into LoaiSP values 
+( 'lsp001' ,N' CPU - Bộ vi xử l&yacute; ','' ,'' );
+insert into LoaiSP values 
+( 'lsp002',N'Mainboard - Bo mạch chủ ', '','' );
+insert into LoaiSP values 
+( 'lsp003', N'RAM - Bộ nhớ trong' ,'' , '');
+insert into LoaiSP values 
+('lsp004', N'Ổ cứng HDD' , '', '');
+insert into LoaiSP values 
+( 'lsp005', N'Ổ cứng SSD', '', '');
+insert into LoaiSP values 
+('lsp006',N' ODD - Ổ đĩa quang' , '', '');
+insert into LoaiSP values 
+('lsp007',N' VGA - Card M&agrave;n H&igrave;nh' ,'' , '');
+insert into LoaiSP values 
+('lsp008',N' Case - Vỏ m&aacute;y t&iacute;nh ','' ,'' );
+insert into LoaiSP values 
+ ( 'lsp009', N'PSU - Nguồn m&aacute;y t&iacute;nh','' , '');
+
+-- phieu nhap: MaPN, NgayNhap, MaNCC
+insert into PhieuNhap values
+( 'pn1', '2020-2-3', 'ncc1');
+insert into PhieuNhap values
+( 'pn2', '2020-5-4', 'ncc2');
+insert into PhieuNhap values
+( 'pn3', '2020-4-9', 'ncc3');
+
+-- nha cc: MaNCC, TenNCC, DiaChi, SDT, Email
+insert into NhaCC values
+( 'ncc1', N'Le Duong Electronics', N'số 56 Hoàng Hoa Thám, Tây Hồ, Hà Nội (Mặt đường, đối diện trường tiểu học Ba Đình)', '024 2421 882','');
+insert into NhaCC values
+( 'ncc2', N'Cửa hàng linh kiện điện tử Hoàng Phát', N'Số 9-B1-Thủ lệ 1-Ngọc khánh-Ba đình-HN (đi ngõ 113 Đào tấn)', '0834.661158 ', 'dientuhoangphat@gmail.com');
+insert into NhaCC values
+( 'ncc3', N'Công ty TNHH điện tử công nghiệp Ritech', N'Số 43, Ngõ 467 Phạm Văn Đồng, Cổ Nhuế, Từ Liêm, Hà Nội', '0902 187 499', 'ritech.vn@gmail.com');
+
+-- khach hang: MaKH, TenKH, SDT, GioiTinh, Tuoi, DiaChi, UserName, Email, Pass, IsVip
+insert into KhachHang values 
+( 'kh1', N'Bùi Hoàng Lam', '0367035548', N'Nam', '22', N'Ngãi Cầu - An Khánh - Hoài Đức - Hà Nội', 'buihoanglam', 'buihoanglam99@gmail.com', '123456', 'true');
+insert into KhachHang values 
+( 'kh2', N'Nguyễn Duy Đạt', '', N'Nam', '22', '', 'nguyenduydat', 'dat@gmail.com', '123456', 'false');
+insert into KhachHang values 
+( 'kh3', N'Trần Văn Linh', '', N'Nam', '22', '', 'tranvanlinh', 'linh@gmail.com', '123456', 'false');
+
+-- admin: MaAdmin, UserAd, Email, Pass 
+insert into Admin values
+( 'ad1', 'lam', 'buihoanglam99@gmail.com', '123456');
+insert into Admin values
+( 'ad2', 'test', 'test@gmail.com', '123456');
+insert into Admin values
+( 'ad3', 'linh', 'linh@gmail.com', '123456');
+insert into Admin values
+( 'ad4', 'dat', 'dat@gmail.com', '123456');
+
+-- quyen: MaQuyen, TenQuyen
+insert into Quyen values
+( 'mq1', 'admin');
+insert into Quyen values
+( 'mq2', 'quanly');
+insert into Quyen values
+( 'mq3', 'nhanvien');
+
+-- quyen ad: MaAdmin, MaQuyen, GhiChu
+insert into QuyenAd values
+( 'ad1', 'mq1', '');
+insert into QuyenAd values
+( 'ad2', 'mq2', '');
+insert into QuyenAd values
+( 'ad3', 'mq3', '');
+insert into QuyenAd values
+( 'ad4', 'mq1', '');
+
+-- chi tiet phieu nhap: MaPN, MaSP, SoLuong, DonGia
+insert into CTPhieuNhap values
+( 'pn1', 'sp1', '10', '11999000');
+insert into CTPhieuNhap values
+( 'pn2', 'sp2', '3','22999000');
+insert into CTPhieuNhap values
+( 'pn2', 'sp3', '6','11999000');
+insert into CTPhieuNhap values
+( 'pn2', 'sp4', '8','14000000');
+insert into CTPhieuNhap values
+( 'pn3', 'sp5', '9','11000000');
