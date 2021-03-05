154c794c91779d0334ea313566b5766aa6c6b8db
TranLinhcbx9 <60964536+TranLinhcbx9@users.noreply.github.com>
2021-03-05 23:33:23 +0700

    admin
    layoutadmin

--- dat/Eshopper/Eshopper/Areas/Admin/Controllers/SanPhamController.cs
+++ dat/Eshopper/Eshopper/Areas/Admin/Controllers/SanPhamController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Eshopper.Models;
 
 namespace Eshopper.Areas.Admin.Controllers
 {
@@ -14,76 +16,83 @@ namespace Eshopper.Areas.Admin.Controllers
             return View();
         }
 
-        // GET: Admin/SanPham/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
+        //public ActionResult GetData()
+        //{
+        //    using (DbModel db = new DbModel())
+        //    {
 
-        // GET: Admin/SanPham/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
+        //    }
+        //}
+        //// GET: Admin/SanPham/Details/5
+        //public ActionResult Details(int id)
+        //{
+        //    return View();
+        //}
 
-        // POST: Admin/SanPham/Create
-        [HttpPost]
-        public ActionResult Create(FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
+        //// GET: Admin/SanPham/Create
+        //public ActionResult Create()
+        //{
+        //    return View();
+        //}
 
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
+        //// POST: Admin/SanPham/Create
+        //[HttpPost]
+        //public ActionResult Create(FormCollection collection)
+        //{
+        //    try
+        //    {
+        //        // TODO: Add insert logic here
 
-        // GET: Admin/SanPham/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
+        //        return RedirectToAction("Index");
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
 
-        // POST: Admin/SanPham/Edit/5
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
+        //// GET: Admin/SanPham/Edit/5
+        //public ActionResult Edit(int id)
+        //{
+        //    return View();
+        //}
 
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
+        //// POST: Admin/SanPham/Edit/5
+        //[HttpPost]
+        //public ActionResult Edit(int id, FormCollection collection)
+        //{
+        //    try
+        //    {
+        //        // TODO: Add update logic here
 
-        // GET: Admin/SanPham/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
+        //        return RedirectToAction("Index");
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
 
-        // POST: Admin/SanPham/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
+        //// GET: Admin/SanPham/Delete/5
+        //public ActionResult Delete(int id)
+        //{
+        //    return View();
+        //}
 
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
+        //// POST: Admin/SanPham/Delete/5
+        //[HttpPost]
+        //public ActionResult Delete(int id, FormCollection collection)
+        //{
+        //    try
+        //    {
+        //        // TODO: Add delete logic here
+
+        //        return RedirectToAction("Index");
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
     }
 }
