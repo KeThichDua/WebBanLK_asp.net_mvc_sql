684aa47ba2c8191801b8ea51365d59f8b08de3f0
TranLinhcbx9 <60964536+TranLinhcbx9@users.noreply.github.com>
2021-03-05 18:10:27 +0700

    AdminInterface
    Giao diện quản lí admin

---
+++ dat/Eshopper/Eshopper/Areas/Admin/Controllers/NhaCungCapController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Eshopper.Areas.Admin.Controllers
+{
+    public class NhaCungCapController : Controller
+    {
+        // GET: Admin/NhaCungCap
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Admin/NhaCungCap/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Admin/NhaCungCap/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/NhaCungCap/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Admin/NhaCungCap/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Admin/NhaCungCap/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Admin/NhaCungCap/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Admin/NhaCungCap/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
